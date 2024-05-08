8e0377872c97ab8b405b104fc6c600903cda1d66
MahendraSingh10 <148310827+MahendraSingh10@users.noreply.github.com>
2024-05-08 17:39:40 +0530

    Version Change to 1.2

--- image-preview/src/main/java/com/plcoding/image_preview/ImagePreview.kt
+++ image-preview/src/main/java/com/plcoding/image_preview/ImagePreview.kt
@@ -4,11 +4,16 @@ import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -73,5 +78,107 @@ fun ShowCustomMessage(context: Context, message: String){
 
 @Composable
 fun ShowCustomTextBox(desc: String){
-    Text(text = desc, modifier = Modifier.size(150.dp).height(20.dp).width(100.dp))
+    Text(text = desc, modifier = Modifier
+        .size(150.dp)
+        .height(20.dp)
+        .width(100.dp))
+}
+
+
+@Composable
+fun generateForm(context:Context,
+                 onFormSubmitCallback: (Context) -> Unit = {},
+                 onFormbackCallback: (Context) -> Unit = {}){
+
+    Column(
+        modifier = Modifier
+            .border(5.dp, color = Color.Magenta)
+            .padding(16.dp, 16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Spacer(modifier = Modifier.padding(4.dp))
+
+        Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+
+                Text(
+                    text = "Consumer Name",
+                    style = MaterialTheme.typography.h3
+                )
+
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = "",
+                    onValueChange = { it },
+                    placeholder = { Text(text = "e.g. Mahendra Singh") },
+                )
+            }
+        }
+        Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+
+                Text(
+                    text = "Business Partner",
+                    style = MaterialTheme.typography.h3
+                )
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = "",
+                    onValueChange = { it },
+                    placeholder = { Text(text = "e.g. Business Partner") },
+                )
+            }
+        }
+        Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+
+                Text(
+                    text = "Address",
+                    style = MaterialTheme.typography.h3
+                )
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = "",
+                    onValueChange = { it },
+                    placeholder = { Text(text = "e.g. Address") },
+                )
+            }
+        }
+        Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Button(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .align(Alignment.CenterHorizontally),
+                    onClick = { onFormSubmitCallback(context) }) {
+                    Text(
+                        text = "Submit",
+                        style = MaterialTheme.typography.button
+                    )
+                }
+                Button(modifier = Modifier
+                    .width(150.dp)
+                    .height(56.dp)
+                    .align(Alignment.CenterHorizontally),
+                    onClick = { onFormbackCallback(context) }, ) {
+                    Text(
+                        text = "Back",
+                        style = MaterialTheme.typography.button
+                    )
+                }
+            }
+
+        }
+    }
+
 }
